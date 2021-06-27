957ba5e25b302bc6467d69a19c6aaa9e0f015f53
arturadamian <arturadamian@gmail.com>
2021-06-26 22:17:53 -0700

    Add new Scenario with steps

--- behave_ebay/steps/definitions.py
+++ behave_ebay/steps/definitions.py
@@ -1,4 +1,6 @@
 import shutil
+from time import time
+from time import sleep
 from pathlib import Path
 from grappa import should
 from selenium import webdriver
@@ -13,9 +15,18 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 def open_element_in_new_tab(context, xpath_or_element):
+    """Open element in a new tab and switch to it.
+
+    Args:
+        context (obj): Behave object.
+        xpath_or_element (str or obj): xpath locator or WebDriver element.
+
+    """
     if isinstance(xpath_or_element, str):
-        element = context.wait.until(EC.element_to_be_clickable(
-            (By.XPATH, xpath_or_element)))
+        element = WebDriverWait(
+            context.driver, context.delay).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, xpath_or_element)))
     else:
         element = xpath_or_element
     context.window_before = context.driver.window_handles[0]
@@ -24,16 +35,81 @@ def open_element_in_new_tab(context, xpath_or_element):
         element).key_down(
         Keys.COMMAND).click().key_up(
         Keys.COMMAND).perform()
+    WebDriverWait(context.driver, context.delay).until(
+        EC.number_of_windows_to_be(2))
     window_after = context.driver.window_handles[1]
     context.driver.switch_to_window(window_after)
+
+
+def test_carousel_left(context, left):
+
+    if context.count == 0:
+        context.count = 2
+    else:
+        context.count -= 1
+    left.click()
+    sleep(1)
+    slide = context.hero_carousel_slides[context.count]
+    slide_apearence = slide.get_attribute("aria-hidden")
+
+    slide_apearence | should.be.equal.to(None)
+
+
+def test_carousel_right(context, right):
+    if context.count == 2:
+        context.count = 0
+    else:
+        context.count += 1
+    right.click()
+    sleep(1)
+    slide = context.hero_carousel_slides[context.count]
+    slide_apearence = slide.get_attribute("aria-hidden")
+
+    slide_apearence | should.be.equal.to(None)
+
+
+def test_carousel_play_pause(context, play_pause):
+    context.xpath_play_pause_class = \
+        "//*[name() = 'svg' and (contains(@class, 'icon--play') " \
+        "or contains(@class, 'icon-pause'))]"
+
+    play_pause_class = WebDriverWait(
+        context.driver, context.delay).until(
+        EC.presence_of_element_located(
+            (By.XPATH, context.xpath_play_pause_class)))
+    status_class = play_pause_class.get_attribute("class")
+    play, pause = 0, 0
+    if "icon--play" in status_class:
+        play = 1
+    elif "icon--pause" in status_class:
+        pause = 1
+    if context.count == 2 and play == 1:
+        context.count = 0
+    elif play == 1:
+        context.count += 1
+    elif pause == 1:
+        play_pause.click()
+        sleep(1)
+        return
+    play_pause.click()
+    sleep(1)
+    slide = context.hero_carousel_slides[context.count]
+    slide_apearence = slide.get_attribute("aria-hidden")
+
+    slide_apearence | should.be.equal.to(None)
 
 
 @given('Open eBay')
 def open_ebay(context):
     context.driver = webdriver.Chrome()
     context.driver.get("https://www.ebay.com/")
-    context.wait = WebDriverWait(context.driver, 10)
-    context.driver.implicitly_wait(5)
+    context.delay = 5
+
+
+@then('Quit browser')
+def quit_browser(context):
+    context.driver.close()
+    context.driver.quit()
 
 
 @when('Choose "{category}"')
@@ -42,8 +118,10 @@ def choose_category(context, category):
     element = ''
 
     try:
-        element = context.wait.until(EC.presence_of_element_located(
-            (By.XPATH, xpath_select_category)))
+        element = WebDriverWait(
+            context.driver, context.delay).until(
+            EC.presence_of_element_located(
+                (By.XPATH, xpath_select_category)))
     except TimeoutException:
         print("Cannot choose the category")
     finally:
@@ -55,20 +133,27 @@ def choose_category(context, category):
 def search_item(context, item):
     xpath_search_field = "//input[@id = 'gh-ac']"
 
-    search_input = context.wait.until(
-        EC.presence_of_element_located((By.XPATH, xpath_search_field)))
+    search_input = WebDriverWait(
+        context.driver, context.delay).until(
+        EC.presence_of_element_located(
+            (By.XPATH, xpath_search_field)))
     search_input.send_keys(item)
-    context.wait.until(
+    WebDriverWait(context.driver, context.delay).until(
         lambda browser: search_input.get_attribute('value') == item)
 
 
 @when('Press search button')
 def press_search_button(context):
     xpath_search_button = "//input[@id = 'gh-btn']"
-    context.driver.find_element(By.XPATH, xpath_search_button).click()
+
+    element = WebDriverWait(
+        context.driver, context.delay).until(
+        EC.element_to_be_clickable(
+            (By.XPATH, xpath_search_button)))
+    element.click()
 
 
-@when('Find first item with "Buy it now" option and add it to cart')
+@when('Find first item with "Buy it now" option')
 def add_to_cart(context):
     xpath_first_item_with_buy_option = \
         "(//div[contains(@class, 's-item__info')" \
@@ -83,11 +168,14 @@ def add_to_cart(context):
     xpath_item_title = '//h1[@id = "itemTitle"]'
     xpath_add_to_cart_button = "//a[@id = 'atcRedesignId_btn']"
 
-    context.item_title = context.wait.until(
+    context.item_title = WebDriverWait(
+        context.driver, context.delay).until(
         EC.presence_of_element_located(
             (By.XPATH, xpath_item_title))
     ).text
-    add_to = context.wait.until(EC.element_to_be_clickable(
+    add_to = WebDriverWait(
+        context.driver, context.delay
+    ).until(EC.element_to_be_clickable(
         (By.XPATH, xpath_add_to_cart_button)))
     add_to.click()
     context.driver.close()
@@ -99,12 +187,16 @@ def check_cart(context):
     xpath_title_in_minicart = "//span[parent::div/@class = 'gh-info__title']"
     xpath_cart = "//li[@id = 'gh-minicart-hover']"
 
-    mini_cart = context.wait.until(EC.element_to_be_clickable(
+    mini_cart = WebDriverWait(
+        context.driver, context.delay
+    ).until(EC.element_to_be_clickable(
         (By.XPATH, xpath_cart)))
     actions = ActionChains(context.driver)
     actions.move_to_element(mini_cart).perform()
-    title_in_mini_cart = context.wait.until(EC.element_to_be_clickable(
-        (By.XPATH, xpath_title_in_minicart)))
+    title_in_mini_cart = WebDriverWait(
+        context.driver, context.delay).until(
+        EC.element_to_be_clickable(
+            (By.XPATH, xpath_title_in_minicart)))
 
     title_in_mini_cart.text | should.be.equal(context.item_title)
 
@@ -113,38 +205,53 @@ def check_cart(context):
 # # # # # # # # # # # # SECOND SCENARIO  # # # # # # # # # # # # # # #
 ######################################################################
 
-@when('Verify "{item}" search and sort listings by auctions')
-def verify_and_sort(context, item):
+@then('Verify "{item}" search')
+def verify_searched_item(context, item):
     xpath_header_one = "//h1[@class = 'srp-controls__count-heading']"
-    xpath_sort_listings_auction = "//span[text() = 'Auction' and ancestor::ul[@class='fake-tabs__items']]"
 
-    check = context.wait.until(EC.text_to_be_present_in_element(
-        (By.XPATH, xpath_header_one), item))
+    check = WebDriverWait(context.driver, context.delay).until(
+        EC.text_to_be_present_in_element(
+            (By.XPATH, xpath_header_one), item))
+
     check | should.be.equal.to(True)
 
+
+@when('Sort listings by left "{option}"')
+def sort_listings_by_left_option(context, option):
+    xpath_sort_listings_option = f"//span[text() = '{option}'" \
+                                  f" and ancestor::ul[" \
+                                  f"@class='fake-tabs__items']]"
+
     try:
-        element = context.wait.until(EC.element_to_be_clickable(
-            (By.XPATH, xpath_sort_listings_auction)))
+        element = WebDriverWait(
+            context.driver, context.delay).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, xpath_sort_listings_option)))
         element.click()
     except TimeoutException:
-        print("Cannot sort Listings by Action")
+        print(f"Cannot sort Listings by {option}")
 
 
-@when('Filter items: {price_max}, {price_min}, {ship_price_max}, {bid_days_left}')
-def filter_items(context, price_max, price_min, ship_price_max, bid_days_left):
-    xpath_links_for_filtered_items = "//div[contains(@class, 's-item__details')" \
-                                          " and (translate(.//span/text(), '$', '') > 20" \
-                                          " and translate(.//span/text(), '$', '') < 26 )" \
-                                          " and (.//span[(contains(text(),'Free shipping'))" \
-                                          " or translate(substring-before(., ' shipping'" \
-                                          "), '+$', '') <= 20]) and (.//span[@class='s-item__time-left'" \
-                                          " and substring-before(., 'd') > 1])]/ancestor::div/a"
+@when('Filter items: {price_max}, {price_min}, '
+      '{ship_price_max}, {bid_days_left}')
+def filter_items(context, price_max, price_min,
+                 ship_price_max, bid_days_left):
+    xpath_links_for_filtered_items = \
+        "//div[contains(@class, 's-item__details') and " \
+        "(translate(.//span/text(), '$', '') > 10 and " \
+        "translate(.//span/text(), '$', '') < 30 ) and " \
+        "(.//span[(contains(text(),'Free shipping')) or " \
+        "translate(substring-before(., ' shipping'), '+$', '') <= 20]) and " \
+        "(.//span[@class='s-item__time-left' and " \
+        "substring-before(., 'd') > 1])]/ancestor::div/a"
 
     try:
-        context.watch_links = context.wait.until(EC.presence_of_all_elements_located(
-            (By.XPATH, xpath_links_for_filtered_items)))
+        context.watch_links = WebDriverWait(
+            context.driver, context.delay).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, xpath_links_for_filtered_items)))
     except TimeoutException:
-        print("Cannot gather filtered elements")
+        print("Cannot collect filtered items")
 
 
 @then('Create/cleanup the directory for screenshots')
@@ -153,12 +260,12 @@ def create_dir(context):
     Path("./screenshots").mkdir(parents=True, exist_ok=True)
 
 
-@then('Save screenshots of selected items in a newly created directory')
+@then('Save screenshots of selected items in the directory')
 def save_screenshots(context):
-
     for count, watch_link in enumerate(context.watch_links):
         open_element_in_new_tab(context, watch_link)
-        context.driver.save_screenshot(f"./screenshots/selected_item_picture_" + str(count) + ".png")
+        context.driver.save_screenshot(f"./screenshots/selected_item_picture_"
+                                       + str(count) + ".png")
         context.driver.close()
         context.driver.switch_to.window(context.window_before)
 
@@ -166,3 +273,86 @@ def save_screenshots(context):
 ######################################################################
 # # # # # # # # # # # #  THIRD SCENARIO  # # # # # # # # # # # # # # #
 ######################################################################
+
+@when('Go back')
+def go_back(context):
+    context.driver.back()
+
+
+@when('Find the first Recent searches element in suggested search')
+def find_suggested_one(context):
+    xpath_suggested_one = "(//li[contains(@class, 'ui-menu-item')]/a)[1]"
+
+    context.suggested_one = WebDriverWait(
+        context.driver, context.delay).until(
+        EC.presence_of_element_located(
+            (By.XPATH, xpath_suggested_one)))
+
+
+@then('verify first Recent searches element == "{full_item}"')
+def verify_suggested_recent(context, full_item):
+    suggested_one_aria_label = \
+        context.suggested_one.get_attribute("aria-label")
+
+    suggested_one_aria_label | should.have.contain("Recent searches")
+    suggested_one_aria_label | should.have.contain(full_item)
+
+
+#####################################################################
+# # # # # # # # # # # #  FOURTH SCENARIO  # # # # # # # # # # # # # #
+#####################################################################
+
+@then('Verify carousel autoplay')
+def find_carousel_elements(context):
+    xpath_hero_carousel_slides = \
+        "//li[contains(@class, 'carousel__snap-point')" \
+        " and (ancestor::div[contains(@class, 'carousel__autoplay')])]"
+    xpath_hero_carousel_play_pause_button = \
+        "//button[contains(@class, 'carousel__playback')]"
+
+    context.hero_carousel_slides = WebDriverWait(
+        context.driver, context.delay).until(
+        EC.presence_of_all_elements_located(
+            (By.XPATH, xpath_hero_carousel_slides)))
+
+    context.count = 0
+    for slide in context.hero_carousel_slides:
+        slide_apearence = slide.get_attribute("aria-hidden")
+        slide_apearence | should.be.equal.to(None)
+        if context.count != 2:
+            sleep(3.3)
+        else:
+            hero_carousel_play_pause_button = WebDriverWait(
+                context.driver, context.delay).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, xpath_hero_carousel_play_pause_button)))
+            hero_carousel_play_pause_button.click()
+            break
+        context.count += 1
+    hero_carousel_play_pause_button = WebDriverWait(
+        context.driver, context.delay).until(
+        EC.element_to_be_clickable(
+            (By.XPATH, xpath_hero_carousel_play_pause_button)))
+    test_carousel_play_pause(context, hero_carousel_play_pause_button)
+
+
+@then('Verify carousel controls - left, right, play/pause buttons')
+def verify_carousel_conrtols(context):
+    xpath_hero_carousel_left_arrow_button = \
+        "(//button[contains(@class, 'carousel__control') " \
+        "and (ancestor::div[contains(@class, 'carousel__autoplay')])])[1]"
+    xpath_hero_carousel_right_arrow_button = \
+        "(//button[contains(@class, 'carousel__control')" \
+        " and (ancestor::div[contains(@class, 'carousel__autoplay')])])[2]"
+
+    hero_carousel_left_arrow_button = WebDriverWait(
+        context.driver, context.delay).until(
+        EC.element_to_be_clickable(
+            (By.XPATH, xpath_hero_carousel_left_arrow_button)))
+    test_carousel_left(context, hero_carousel_left_arrow_button)
+
+    hero_carousel_right_arrow_button = WebDriverWait(
+        context.driver, context.delay).until(
+        EC.element_to_be_clickable(
+            (By.XPATH, xpath_hero_carousel_right_arrow_button)))
+    test_carousel_right(context, hero_carousel_right_arrow_button)
--- behave_ebay/test.feature
+++ behave_ebay/test.feature
@@ -1,28 +1,61 @@
 Feature: eBay Regression Testing
 
-  Scenario Outline: Add the first searched item to cart
+  Scenario Outline: Add the first searched item to cart and verify that
     Given Open eBay
     When Choose "<category>"
     And Type "<item>" in the search field
     And Press search button
-    And Find first item with "Buy it now" option and add it to cart
+    And Find first item with "Buy it now" option
     Then Add the item to cart and get the title
     Then Check if the item is in the cart
+    And Quit browser
 
     Examples:
       | category | item   |
       | Antiques | Lamp   |
       | Books    | Python |
 
-  Scenario Outline: Search with suggestion and watch filtered items on auction
+  Scenario Outline: Search specific items on auction and save pictures of them
     Given Open eBay
     When Type "<item>" in the search field
     And Press search button
-    And Verify "<item>" search and sort listings by auctions
+    Then Verify "<item>" search
+    When Sort listings by left "<option>"
     And Filter items: "<price_max>", "<price_min>", "<ship_price_max>", "<bid_days_left>"
     Then Create/cleanup the directory for screenshots
-    Then Save screenshots of selected items in a newly created directory
+    Then Save screenshots of selected items in the directory
+    And Quit browser
 
     Examples:
-      | item        | price_max | price_min | ship_price_max | bid_days_left |
-      | shoes women | 25        | 20        | 10             | 1             |
+      | item        | price_max | price_min | ship_price_max | bid_days_left | option  |
+      | shoes women | 25        | 20        | 10             | 1             | Auction |
+
+  Scenario Outline: Verify Recent searches in suggested search menu
+    Given Open eBay
+    When Type "<full_item>" in the search field
+    And Press search button
+    And Go back
+    When Type "<partial_item>" in the search field
+    And Find the first Recent searches element in suggested search
+    Then Verify first Recent searches element == "<full_item>"
+    And Quit browser
+
+    Examples:
+      | partial_item | full_item   |
+      | shoe         | shoes women |
+
+
+  Scenario: Verify Hero carousel functionality
+    Given Open eBay
+    Then Verify carousel autoplay
+    Then Verify carousel controls - left, right, play/pause buttons
+    And Quit browser
+
+  Scenario: Verify image rendering
+    Given Open eBay
+    When Go to advanced search
+    And Select options
+    And Click submit
+    Then Verify the correct search results
+    When Open the first found item
+    And Verify the images are downloading and rendering correctly
