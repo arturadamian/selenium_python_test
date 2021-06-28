3aa9ed027614b17d6ed453b7f6d95b7c392e0624
arturadamian <arturadamian@gmail.com>
2021-06-28 00:16:27 -0700

    Refactor second and third Scenarios in feature file

--- behave_ebay/steps/definitions.py
+++ behave_ebay/steps/definitions.py
@@ -14,31 +14,31 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def open_element_in_new_tab(context, xpath_or_element):
-    """Open element in a new tab and switch to it.
-
-    Args:
-        context (obj): Behave object.
-        xpath_or_element (str or obj): xpath locator or WebDriver element.
-
-    """
-    if isinstance(xpath_or_element, str):
-        element = WebDriverWait(
-            context.driver, context.delay).until(
-            EC.element_to_be_clickable(
-                (By.XPATH, xpath_or_element)))
-    else:
-        element = xpath_or_element
-    context.window_before = context.driver.window_handles[0]
-    actions = ActionChains(context.driver)
-    actions.move_to_element(
-        element).key_down(
-        Keys.COMMAND).click().key_up(
-        Keys.COMMAND).perform()
-    WebDriverWait(context.driver, context.delay).until(
-        EC.number_of_windows_to_be(2))
-    window_after = context.driver.window_handles[1]
-    context.driver.switch_to_window(window_after)
+# def open_element_in_new_tab(context, xpath_or_element):
+#     """Open element in a new tab and switch to it.
+#
+#     Args:
+#         context (obj): Behave object.
+#         xpath_or_element (str or obj): xpath locator or WebDriver element.
+#
+#     """
+#     if isinstance(xpath_or_element, str):
+#         element = WebDriverWait(
+#             context.driver, context.delay).until(
+#             EC.element_to_be_clickable(
+#                 (By.XPATH, xpath_or_element)))
+#     else:
+#         element = xpath_or_element
+#     context.window_before = context.driver.window_handles[0]
+#     actions = ActionChains(context.driver)
+#     actions.move_to_element(
+#         element).key_down(
+#         Keys.COMMAND).click().key_up(
+#         Keys.COMMAND).perform()
+#     WebDriverWait(context.driver, context.delay).until(
+#         EC.number_of_windows_to_be(2))
+#     window_after = context.driver.window_handles[1]
+#     context.driver.switch_to_window(window_after)
 
 
 ###############################h#################################
@@ -68,7 +68,7 @@ def autoplay_verify_carousel(context):
         slide_apearence = slide.get_attribute("aria-hidden")
         slide_apearence | should.be.equal.to(None)
         if context.count != 2:
-            sleep(3.3)
+            sleep(3.2)
         else:
             sleep(1)
             break
@@ -157,25 +157,34 @@ def verify_carousel_right(context):
     hero_carousel_right_arrow_button.click()
 
 
-###############################################################
-# # # # # # # # # # # # SCENARIO  # # # # # # # # # # # # # # #
+######################################################
+# # # # # # # # # # #  SCENARIO  # # # # # # # # # # #
 
 # Add the first searched item to cart and verify that
+# with it's title in mini cart
 
-###############################################################
+#######################################################
 
-@when('Choose "{category}"')
+
+@given('Xpath saved in context')
+def save_xpath_context(context):
+    context.xpath_select_category = "//select[@id = 'gh-cat']"
+    context.xpath_search_field = "//input[@id = 'gh-ac']"
+    context.xpath_search_button = "//input[@id = 'gh-btn']"
+    context.xpath_item_title = '//h1[@id = "itemTitle"]'
+
+
+@when('Select "{category}"')
 def choose_category(context, category):
-    xpath_select_category = "//select[@id = 'gh-cat']"
     element = ''
 
     try:
         element = WebDriverWait(
             context.driver, context.delay).until(
             EC.presence_of_element_located(
-                (By.XPATH, xpath_select_category)))
+                (By.XPATH, context.xpath_select_category)))
     except TimeoutException:
-        print("Cannot choose the category")
+        print(f"Cannot select {category} category")
     finally:
         select = Select(element)
         select.select_by_visible_text(category)
@@ -183,12 +192,11 @@ def choose_category(context, category):
 
 @when('Type "{item}" in the search field')
 def search_item(context, item):
-    xpath_search_field = "//input[@id = 'gh-ac']"
 
     search_input = WebDriverWait(
         context.driver, context.delay).until(
         EC.presence_of_element_located(
-            (By.XPATH, xpath_search_field)))
+            (By.XPATH, context.xpath_search_field)))
     search_input.send_keys(item)
     WebDriverWait(context.driver, context.delay).until(
         lambda browser: search_input.get_attribute('value') == item)
@@ -196,47 +204,71 @@ def search_item(context, item):
 
 @when('Press search button')
 def press_search_button(context):
-    xpath_search_button = "//input[@id = 'gh-btn']"
 
     element = WebDriverWait(
         context.driver, context.delay).until(
         EC.element_to_be_clickable(
-            (By.XPATH, xpath_search_button)))
+            (By.XPATH, context.xpath_search_button)))
     element.click()
 
 
-@when('Find first item with "Buy it now" option')
+@when('Find first item to open in a new tab')
 def add_to_cart(context):
     xpath_first_item_with_buy_option = \
         "(//div[contains(@class, 's-item__info')" \
         " and .//span/text() = 'Buy It Now']/a)[1]"
 
-    open_element_in_new_tab(
-        context, xpath_first_item_with_buy_option)
+    context.xpath_for_new_tab = xpath_first_item_with_buy_option
 
 
-@then('Add the item to cart and get the title')
+@when('Open element in a new tab')
+def open_element_in_new_tab(context):
+    if isinstance(context.xpath_el_new_tab, str):
+        element = WebDriverWait(
+            context.driver, context.delay).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, context.xpath_for_new_tab)))
+    else:
+        element = context.xpath_el_new_tab
+    context.window_before = context.driver.window_handles[0]
+    actions = ActionChains(context.driver)
+    actions.move_to_element(
+        element).key_down(
+        Keys.COMMAND).click().key_up(
+        Keys.COMMAND).perform()
+    WebDriverWait(context.driver, context.delay).until(
+        EC.number_of_windows_to_be(2))
+    window_after = context.driver.window_handles[1]
+    context.driver.switch_to_window(window_after)
+
+
+@then('Get the title of the item')
 def add_to_cart(context):
-    xpath_item_title = '//h1[@id = "itemTitle"]'
-    xpath_add_to_cart_button = "//a[@id = 'atcRedesignId_btn']"
-
     context.item_title = WebDriverWait(
         context.driver, context.delay).until(
         EC.presence_of_element_located(
-            (By.XPATH, xpath_item_title))
+            (By.XPATH, context.xpath_item_title))
     ).text
-    add_to = WebDriverWait(
-        context.driver, context.delay
-    ).until(EC.element_to_be_clickable(
-        (By.XPATH, xpath_add_to_cart_button)))
-    add_to.click()
+
+
+@then('Add the item to cart if there is an option')
+def add_to_cart(context):
+    xpath_add_to_cart_button = "//a[@id = 'atcRedesignId_btn']"
+
+    try:
+        add_to_button = WebDriverWait(
+            context.driver, context.delay
+        ).until(EC.element_to_be_clickable(
+            (By.XPATH, xpath_add_to_cart_button)))
+        add_to_button.click()
+    except TimeoutException:
+        context.scenario.skip(reason='This item has no "Add to cart" option')
     context.driver.close()
     context.driver.switch_to.window(context.window_before)
 
 
-@then('Check if the item is in the cart')
-def check_cart(context):
-    xpath_title_in_minicart = "//span[parent::div/@class = 'gh-info__title']"
+@when('Hover over cart')
+def hover_over_cart(context):
     xpath_cart = "//li[@id = 'gh-minicart-hover']"
 
     mini_cart = WebDriverWait(
@@ -245,17 +277,36 @@ def check_cart(context):
         (By.XPATH, xpath_cart)))
     actions = ActionChains(context.driver)
     actions.move_to_element(mini_cart).perform()
+
+
+@then('Verify the title of the item in mini cart')
+def verify_item_in_cart(context):
+    xpath_title_in_minicart = "//span[parent::div/@class = 'gh-info__title']"
+
     title_in_mini_cart = WebDriverWait(
         context.driver, context.delay).until(
-        EC.element_to_be_clickable(
+        EC.visibility_of_element_located(
             (By.XPATH, xpath_title_in_minicart)))
 
     title_in_mini_cart.text | should.be.equal(context.item_title)
 
 
-######################################################################
-# # # # # # # # # # # # SECOND SCENARIO  # # # # # # # # # # # # # # #
-######################################################################
+############################################################
+# # # # # # # # # # #  SCENARIO  # # # # # # # # # # # # # #
+
+# Search specific items on auction and save pictures of them
+# in a created directory
+
+############################################################
+
+
+@given('Directory to create')
+def save_data(context):
+    context.xpath_search_field = "//input[@id = 'gh-ac']"
+    context.xpath_search_button = "//input[@id = 'gh-btn']"
+    context.dir = context.text
+    print()
+
 
 @then('Verify "{item}" search')
 def verify_searched_item(context, item):
@@ -268,11 +319,11 @@ def verify_searched_item(context, item):
     check | should.be.equal.to(True)
 
 
-@when('Sort listings by left "{option}"')
+@when('Sort listings by central left "{option}"')
 def sort_listings_by_left_option(context, option):
-    xpath_sort_listings_option = f"//span[text() = '{option}'" \
-                                  f" and ancestor::ul[" \
-                                  f"@class='fake-tabs__items']]"
+    xpath_sort_listings_option = \
+        f"//span[text() = '{option}' and " \
+        f"ancestor::ul[@class='fake-tabs__items']]"
 
     try:
         element = WebDriverWait(
@@ -285,17 +336,18 @@ def sort_listings_by_left_option(context, option):
 
 
 @when('Filter items: {price_max}, {price_min}, '
-      '{ship_price_max}, {bid_days_left}')
+      '{ship_price_max}, {bidding_min_days_left}')
 def filter_items(context, price_max, price_min,
-                 ship_price_max, bid_days_left):
+                 ship_price_max, bidding_min_days_left):
     xpath_links_for_filtered_items = \
-        "//div[contains(@class, 's-item__details') and " \
-        "(translate(.//span/text(), '$', '') > 10 and " \
-        "translate(.//span/text(), '$', '') < 30 ) and " \
-        "(.//span[(contains(text(),'Free shipping')) or " \
-        "translate(substring-before(., ' shipping'), '+$', '') <= 20]) and " \
-        "(.//span[@class='s-item__time-left' and " \
-        "substring-before(., 'd') > 1])]/ancestor::div/a"
+        f"//div[contains(@class, 's-item__details') and " \
+        f"(translate(.//span/text(), '$', '') > {price_min} and " \
+        f"translate(.//span/text(), '$', '') < {price_max}) and " \
+        f"(.//span[(contains(text(),'Free shipping')) " \
+        f"or translate(substring-before(., ' shipping'), '+$', '')" \
+        f" <= {ship_price_max}]) and (.//span[@class='s-item__time-left'" \
+        f" and substring-before(., 'd') > {bidding_min_days_left}])]" \
+        f"/ancestor::div/a"
 
     try:
         context.watch_links = WebDriverWait(
@@ -308,16 +360,19 @@ def filter_items(context, price_max, price_min,
 
 @then('Create/cleanup the directory for screenshots')
 def create_dir(context):
-    shutil.rmtree('./screenshots')
-    Path("./screenshots").mkdir(parents=True, exist_ok=True)
+    if Path(f"./{context.dir}").is_dir():
+        print("It works")
+    # shutil.rmtree('./screenshots')
+    Path(f"./{context.dir}").mkdir(parents=True, exist_ok=True)
 
 
-@then('Save screenshots of selected items in the directory')
+@then('Open items in a new tab and save screenshots in the directory')
 def save_screenshots(context):
     for count, watch_link in enumerate(context.watch_links):
-        open_element_in_new_tab(context, watch_link)
-        context.driver.save_screenshot(f"./screenshots/selected_item_picture_"
-                                       + str(count) + ".png")
+        context.xpath_el_new_tab = watch_link
+        open_element_in_new_tab(context)
+        context.driver.save_screenshot(
+            f"./{context.dir}/selected_item_picture_{count}.png")
         context.driver.close()
         context.driver.switch_to.window(context.window_before)
 
--- behave_ebay/test.feature
+++ behave_ebay/test.feature
@@ -12,45 +12,49 @@ Feature: eBay Regression Testing
     Then Verify carousel correct slide appearance
 
 
-  Scenario Outline: Add the first searched item to cart and verify that
-    Given
-    When Choose "<category>"
+  Scenario Outline: Add the first searched item to cart and verify that with it's title in mini cart
+    Given Xpath saved in context
+    When Select "<category>"
     And Type "<item>" in the search field
     And Press search button
-    And Find first item with "Buy it now" option
-    Then Add the item to cart and get the title
-    Then Check if the item is in the cart
-
+    And Find first item to open in a new tab
+    When Open element in a new tab
+    Then Get the title of the item
+    Then Add the item to cart if there is an option
+    When Hover over cart
+    Then Verify the title of the item in mini cart
 
     Examples:
       | category | item   |
       | Antiques | Lamp   |
       | Books    | Python |
 
-  Scenario Outline: Search specific items on auction and save pictures of them
-    Given Open eBay
+  Scenario Outline: Search specific items on auction and save pictures of them in a created directory
+    Given Directory to create
+      """
+      screenshots
+      """
     When Type "<item>" in the search field
     And Press search button
     Then Verify "<item>" search
-    When Sort listings by left "<option>"
-    And Filter items: "<price_max>", "<price_min>", "<ship_price_max>", "<bid_days_left>"
+    When Sort listings by central left "<option>"
+    And Filter items: "<price_max>", "<price_min>", "<ship_price_max>", "<bidding_min_days_left>"
     Then Create/cleanup the directory for screenshots
-    Then Save screenshots of selected items in the directory
+    Then Open items in a new tab and save screenshots in the directory
 
 
     Examples:
-      | item        | price_max | price_min | ship_price_max | bid_days_left | option  |
-      | shoes women | 25        | 20        | 10             | 1             | Auction |
+      | item        | price_max | price_min | ship_price_max | bidding_min_days_left | option  |
+      | shoes women | 25        | 20        | 10             | 1                     | Auction |
 
   Scenario Outline: Verify Recent searches in suggested search menu
-    Given Open eBay
+    Given
     When Type "<full_item>" in the search field
     And Press search button
     And Go back
     When Type "<partial_item>" in the search field
     And Find the first Recent searches element in suggested search
     Then Verify first Recent searches element == "<full_item>"
-    And Quit browser
 
     Examples:
       | partial_item | full_item   |
